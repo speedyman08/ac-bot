e16d13cedd71215a63064b866ff85e382a4384e1
stephen <stephen.jr@selyuk.com>
2023-08-23 19:47:23 +0300

    Upload

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,26 @@
+﻿using DiscordBot.commands;
+using DSharpPlus;
+using DSharpPlus.SlashCommands;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordBot
+{
+    public class DiscordBot
+    {
+        private static async Task Main(string[] args)
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = File.ReadAllText("token.txt"),
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All,
+                MinimumLogLevel = LogLevel.Information,
+            });
+            await discord.ConnectAsync();
+            var slash = discord.UseSlashCommands();
+            slash.RegisterCommands<AcCommands>(1115372571605610757);
+            await Task.Delay(-1);
+        }
+        
+    }
+}
---
+++ DiscordBot/commands/AcCommands.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Http.Headers;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace DiscordBot.commands;
+public class AcCommands : ApplicationCommandModule
+{
+    [SlashCommand("ac-on", "Turns AC on")]
+    public async Task AcOn(InteractionContext e, 
+        [Option("mode", "AC mode (heat,cool,dry)")] string mode,
+        [Option("temperature", "AC temperature (16-31)")] string temperature, 
+        [Option("time", "AC time (multiples of 30)")] string time 
+        ) 
+    {
+        await e.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+        var client = new HttpClient();
+        var response = await client.GetStringAsync($"http://10.50.0.111:5000/homekit/ac-{mode}-{temperature}c-{time}min");
+        await e.EditResponseAsync(new DiscordWebhookBuilder().WithContent(response));
+    }
+    [SlashCommand("ac-off", "Turns AC off")]
+    public async Task AcOff(InteractionContext e)
+    {
+        await e.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource);
+        var client = new HttpClient();
+        var response = await client.GetStringAsync("http://10.50.0.111:5000/homekit/ac-off");
+        await e.EditResponseAsync(new DiscordWebhookBuilder().WithContent(response));
+    }
+}
